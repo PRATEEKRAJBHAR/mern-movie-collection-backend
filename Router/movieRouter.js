79564257efeabd3d80546e8b8b6961f0029863ee
PRATEEKRAJBHAR <prateekrajbhar3311@gmail.com>
2025-12-15 15:34:47 +0530

    something changes

--- Router/movieRouter.js
+++ Router/movieRouter.js
@@ -1,4 +1,3 @@
-// routes/movieRoutes.js
 const express = require("express");
 const router = express.Router();
 const {
@@ -17,7 +16,6 @@ const { checkAdmin } = require('../Middleware/checkAdmin');
 router.post("/register", registerRoute);
 router.post("/login", loginRoutes);
 
-// Import TMDB movies
 
 
 router.get("/import-movies", movieAuth, checkAdmin, importMovies);
@@ -32,45 +30,3 @@ module.exports = router;
 
 
 
-
-// const express = require("express");
-// const router = express.Router();
-
-// const {
-//   getAllMovies,
-//   searchMovies,
-//   sortMovies,
-//   createMovie,
-//   updateMovie,
-//   deleteMovie,
-//   importMovies
-// } = require("../controller/movieController");
-
-// const { movieAuth } = require("../Middleware/movieAuthmiddleware");
-// const { checkAdmin } = require("../Middleware/checkAdmin");
-
-// // ================= MOVIES =================
-// router.post("/register", registerRoute);
-// router.post("/login", loginRoutes);
-// // Get all movies (pagination only)
-// router.get("/movies", movieAuth, getAllMovies);
-
-// // Search movies
-// router.get("/movies/search", movieAuth, searchMovies);
-
-// // Sort movies
-// router.get("/movies/sorted", movieAuth, sortMovies);
-
-// // Create movie (admin)
-// router.post("/movies", movieAuth, checkAdmin, createMovie);
-
-// // Update movie (admin)
-// router.put("/movies/:id", movieAuth, checkAdmin, updateMovie);
-
-// // Delete movie (admin)
-// router.delete("/movies/:id", movieAuth, checkAdmin, deleteMovie);
-
-// // Import TMDB (admin)
-// router.get("/import-movies", movieAuth, checkAdmin, importMovies);
-
-// module.exports = router;
